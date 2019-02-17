5d39ea75beb98d4449aa962dfe072b728411dbb5
wengchuqin <wengchuqin@meituan.com>
2019-02-17 22:51:09 +0800

    调整了单元测试的路径

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 // var assert = require('assert');
 
-var UserServiceImpl = require('./service/UserService.js');
+var UserServiceImpl = require('../service/UserService.js');
 userService = new UserServiceImpl();
 
 // userService.save({
