6e70b24b1653411adf4670a80a77fa683f381569
wengchuqin <wengchuqin@meituan.com>
2019-02-17 18:07:46 +0800

    添加了单元测试 使用命令行运行: npm test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-var UserServiceImpl = require('./service/UserService.js');
+// var assert = require('assert');
 
+var UserServiceImpl = require('./service/UserService.js');
 userService = new UserServiceImpl();
 
 // userService.save({
@@ -7,5 +8,16 @@ userService = new UserServiceImpl();
 //     pwd:'123456'
 // })
 
-var users = userService.findUsers(2, 3);
-console.log(users);
+//
+// var users = userService.findUsers(2, 3);
+// console.log(users);
+
+
+var assert = require('assert');
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1,2,3].indexOf(4), -1);
+        });
+    });
+});
