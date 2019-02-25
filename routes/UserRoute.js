6cb928e9431d9fea4a454abf95febcf71c3160da
Akatsuki2333 <1154178482@qq.com>
2019-02-25 22:09:17 +0800

    添加了 hello 接口

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -17,9 +17,15 @@ router.get('/findAllUsers', function (req, res, next) {
     var users = userService.findUsers(req.query.page, req.query.size);
     res.end( JSON.stringify(users));
 });
+// 通过用户名密码查询用户是否存在
 router.get('/findUsers', function (req, res, next) {
     var user = userService.find(req.query.name, req.query.pwd);
     res.end( JSON.stringify(user));
+});
+
+// 返回hello
+router.get('/hello', function (req, res, next) {
+    res.end( "hello");
 });
 
 
